e86eef1eb934ec6feb2b7006ecb6ab22897c3d0f
Ryan Webb <dzanfox@gmail.com>
2017-04-02 21:57:17 -0500

    installed and set up gulp and bower json files. set up MVC file structure.

---
+++ src/CourseProjectApp/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProjectApp.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.value = "Hello There";
+            return View();
+        }
+    }
+}
