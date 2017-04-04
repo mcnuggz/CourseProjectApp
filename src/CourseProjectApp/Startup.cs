be479b2098a0f9f6e76acac9fe2ad98b4981ed04
Ryan Webb <dzanfox@gmail.com>
2017-04-03 22:27:59 -0500

    reset MVC folders

--- src/CourseProjectApp/Startup.cs
+++ src/CourseProjectApp/Startup.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
-using CourseProjectApp.Models.CSharp;
 
 namespace CourseProjectApp
 {
@@ -37,8 +31,6 @@ namespace CourseProjectApp
         {
             services.AddApplicationInsightsTelemetry(Configuration);
             services.AddMvc();
-            services.AddSingleton<FirstClass>();
-            services.AddTransient<AccessModifiers>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
