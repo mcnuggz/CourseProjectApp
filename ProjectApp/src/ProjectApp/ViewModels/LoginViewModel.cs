e32f3ac657926bb77841ba4f56a62a961a3de68e
Ryan Webb <dzanfox@gmail.com>
2017-04-08 16:04:40 -0500

    Login ViewComponent, ViewModel implemented

---
+++ ProjectApp/src/ProjectApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
